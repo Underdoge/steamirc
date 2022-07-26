68b01e23e490eb1b12e3b07df44e6f0f508e90a5
Eduardo Chapa <eduardo.chapa@gmail.com>
2022-07-26 10:47:05 -0500

    updated source

--- steamirc_service.sh
+++
@@ -1,4 +0,0 @@
-DATE=`date '+%Y-%m-%d %H:%M:%S'`
-echo "Example service started at ${DATE}" | systemd-cat -p info
-export CONFIG_FILE=./config.json
-node /home/eduardo_chapa_gmail_com/steamirc/server.js
